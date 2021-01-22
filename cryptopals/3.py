8d7f104f68e96f262979ce1d86c2e02bb2638831
barlaabhi <52828480+barlaabhi@users.noreply.github.com>
2021-01-22 21:39:07 +0530

    Update 3.py

--- cryptopals/3.py
+++ cryptopals/3.py
@@ -1,17 +1,19 @@
 # Single-byte XOR cipher
 from collections import Counter
-from score import *
+from score import scores
+def hex_to_ascii(st):
+	g2=[]
+	for i in range(0,len(st)-1,2):
+		g2.append(st[i]+st[i+1])
+
+	k=""
+	for i in g2:
+		k += chr(int(i,16))
+	return k
 
 g = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
-g2=[]
-for i in range(0,len(g)-1,2):
-	g2.append(g[i]+g[i+1])
-
-g=""
-for i in g2:
-	g += chr(int(i,16))
-
+g = hex_to_ascii(g)
 sc = {} 
 for j in range(256):
 	r = "".join(chr(ord(i)^j) for i in g)
