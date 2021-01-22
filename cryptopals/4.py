bfa4652b98bfa3e24a164013ab05e0675b0656dc
barlaabhi <52828480+barlaabhi@users.noreply.github.com>
2021-01-22 21:37:40 +0530

    Update 4.py

--- cryptopals/4.py
+++ cryptopals/4.py
@@ -1,18 +1,20 @@
 from score import scores
+def hex_to_ascii(st):
+	g2=[]
+	for i in range(0,len(st)-1,2):
+		g2.append(st[i]+st[i+1])
+
+	k=""
+	for i in g2:
+		k += chr(int(i,16))
+	return k
 
 f = open("4.txt","r")
 e = f.readlines()
 
 sc={}
 for y in e:
-	
-	g2=[]
-	for i in range(0,len(y)-1,2):
-		g2.append(y[i]+y[i+1])
-
-	k=""
-	for i in g2:
-		k += chr(int(i,16))
+	k = hex_to_ascii(y)
 
 	for j in range(256):
 		r = "".join(chr(ord(i)^j) for i in k)
